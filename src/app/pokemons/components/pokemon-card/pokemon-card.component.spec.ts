9c0fcf8fb67026c68ffc0647f86997e86aec50f8
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-09-14 08:11:16 -0600

    Pruebas en AppComponent, App Routes e inicio en PokemonCardComponent

---
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
@@ -0,0 +1,39 @@
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PokemonCardComponent } from './pokemon-card.component';
+import { provideRouter } from '@angular/router';
+import { SimplePokemon } from '../../interfaces';
+
+const mockPokemon: SimplePokemon = {
+  id: '1',
+  name: 'bulbasaur',
+}
+
+describe('PokemonCardComponent', () => {
+
+  let fixture: ComponentFixture<PokemonCardComponent>;
+  let compiled: HTMLElement;
+  let component: PokemonCardComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PokemonCardComponent],
+      providers: [
+        provideRouter([])
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PokemonCardComponent);
+    fixture.componentRef.setInput('pokemon', mockPokemon);
+
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    console.log(compiled);
+    expect(component).toBeTruthy();
+  });
+});
